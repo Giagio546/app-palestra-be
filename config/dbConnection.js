2acbb417acd52192c7da606ee88d0ebb6aa897c9
antonio.albanese <antonio.albanese@acsoftware.it>
2023-11-21 11:33:51 +0100

    changed db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        console.log('Trying to connect to MongoDB at', process.env.CONNECTION_STRING);
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("Database connected: ", connect.connection.host, connect.connection.name)
-    } catch(err) {
+        console.log("trying to connect to db:", process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
+        console.log("Database connected:", connect.connection.host, connect.connection.name);
+    } catch (err) {
         console.log(err);
         process.exit(1);
     }
 };
 
-module.exports = connectDb
+module.exports = connectDb;
