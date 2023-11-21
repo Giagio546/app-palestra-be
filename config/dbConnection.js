22056a1793f03137c12f81e93deb18f8b25415eb
antonio.albanese <antonio.albanese@acsoftware.it>
2023-11-21 12:15:00 +0100

    .

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,18 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        console.log("trying to connect to db:", process.env.CONNECTION_STRING);
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        });
-        console.log("Database connected:", connect.connection.host, connect.connection.name);
-    } catch (err) {
+        const connect = await mongoose.connect("mongodb://app-palestra-mongodb-1:27017/app-palestra");
+        console.log("Database connected: ", connect.connection.host, connect.connection.name)
+    } catch(err) {
         console.log(err);
         process.exit(1);
     }
 };
 
-module.exports = connectDb;
+module.exports = connectDb
